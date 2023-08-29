31e1252be16644a81888cf81d6685c173009a680
binhabib96 <53971051+binhabib96@users.noreply.github.com>
2023-08-29 12:16:39 -0700

    first commit

---
+++ resources/views/UI/table.blade.php
@@ -0,0 +1,91 @@
+
+    
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+
+
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="p-4">
+
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    IP
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Timestamp
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Status
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Counts
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="w-4 p-4">
+
+                </td>
+                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+            
+                    <div class="pl-3">
+                        <div class="text-base font-semibold">10.10.5.5</div>
+
+                    </div>  
+                </th>
+                <td class="px-6 py-4">
+                23-06-26 02:31:29,573
+
+                </td>
+                <td class="px-6 py-4">
+                    <div class="flex items-center">
+                        <div class="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div> Online
+                    </div>
+                </td>
+                <td class="px-6 py-4">
+                    <!-- Modal toggle -->
+                    <div class="pl-3">
+                        <div class="text-base font-semibold">50</div>
+
+                    </div>  
+
+                </td>
+            </tr>
+     <!-- -------------------------------------------------------     -->
+     </td>
+
+     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="w-4 p-4">
+
+                </td>
+                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+            
+                    <div class="pl-3">
+                        <div class="text-base font-semibold">10.10.5.6</div>
+
+                    </div>  
+                </th>
+                <td class="px-6 py-4">
+                23-06-26 03:31:29,573
+
+                </td>
+                <td class="px-6 py-4">
+                    <div class="flex items-center">
+                        <div class="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div> offline
+                    </div>
+                </td>
+                <td class="px-6 py-4">
+                    <!-- Modal toggle -->
+                    <div class="pl-3">
+                        <div class="text-base font-semibold">200</div>
+
+                    </div>  
+
+                </td>
+            </tr>
+            </form>
+        </div>
+    </div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+    <body>
+    <div>@include('UI.table')</div>
+    </body>
+    
+    @vite('resources/js/app.css')
+
+</html>
